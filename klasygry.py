44c1e837b1ecb6401e0329c6ba66ed374a45856d
hbskvrgs <zofiaiwaniukk@gmail.com>
2026-09-08 22:58:59 +0200

    card game classes

--- klasygry.py
+++ klasygry.py
@@ -3,8 +3,25 @@ from enum import Enum
 
 
 class Kolor(Enum):
+    KIER="kier"
+    KARO="karo"
+    TREFL="trefl"
+    PIK="pik"
 
 class Figura(Enum):
+    AS=1
+    DWA=2
+    TRZY=3
+    CZTERY=4
+    PIEC=5
+    SZESC=6
+    SIEDEM=7
+    OSIEM=8
+    DZIEWIEC=9
+    DZIESIEC=10
+    WALET=11
+    DAMA=12
+    KROL=13
 
 class Karta:
 
