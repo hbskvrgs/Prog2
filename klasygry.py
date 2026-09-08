fd5aa61abbee5bca17f2c544cb5359b1afc9ce1a
hbskvrgs <zofiaiwaniukk@gmail.com>
2026-09-08 22:34:58 +0200

    Add files via upload

---
+++ klasygry.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class Kolor(Enum):
+
+class Figura(Enum):
+
+class Karta:
+
+class Talia:
+
+class Stos:
+    
+class StosRoboczy(Stos):
+    
+class StosKoncowy(Stos):
+    
+class StosDobierania(Stos):
+    
