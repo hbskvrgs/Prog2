fde97d7fa7cf156d86b606f6f97ee7499e42615b
hbskvrgs <zofiaiwaniukk@gmail.com>
2026-09-12 23:34:51 +0200

    Add files via upload

---
+++ gra.py
@@ -0,0 +1,10 @@
+from klasygry import *
+
+class Gra:
+    def __init__(self):
+        self.talia = None
+        self.stosy_robocze = []
+        self.stosy_koncowe = {}
+        self.stos_dobierania = None
+        self.stos_odrzuconych = None
+        self.nowa_gra()
