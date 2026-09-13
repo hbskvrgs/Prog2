2c67d922bf773a0ba3d1808d24557d5ea58f7b46
hbskvrgs <zofiaiwaniukk@gmail.com>
2026-09-13 14:41:23 +0200

    methods gra

--- gra.py
+++ gra.py
@@ -8,3 +8,23 @@ class Gra:
         self.stos_dobierania = None
         self.stos_odrzuconych = None
         self.nowa_gra()
+        
+    def nowa_gra(self):
+        
+    def rozdaj_karty(self):
+
+    def dobierz_karte(self):
+
+    def odnow_stos_dobierania(self):
+
+    def przenies_odrzucona_na_roboczy(self):
+
+    def przenies_odrzucona_na_koncowy(self):
+
+    def przenies_roboczy_na_koncowy(self):
+
+    def przenies_roboczy_na_roboczy(self):
+
+    def przenies_koncowy_na_roboczy(self):
+
+    def czy_wygrana(self):
